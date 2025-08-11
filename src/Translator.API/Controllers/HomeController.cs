3e758c7095432f288b274add28b1871eaea96780
shaw3rm5 <shaw3rm5@users.noreply.github.com>
2025-08-11 11:40:49 +0400

    update HomeController.cs and view for him

--- src/Translator.API/Controllers/HomeController.cs
+++ src/Translator.API/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Translator.Application.Features.Dashboard.Queries;
 
 namespace Translator.API.Controllers;
 
 [ApiExplorerSettings(IgnoreApi = true)]
 public class HomeController : Controller
 {
+    private readonly IMediator _mediator;
+
+    public HomeController(IMediator mediator)
+        => _mediator = mediator;
+    
     [HttpGet("")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var command = new GetDashboardStatisticCommand();
+        var statistics = await _mediator.Send(command);
+        return View(statistics);
     }
 }
