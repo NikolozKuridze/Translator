82d1f287d39ea26e2513f3743ee74461fe50cb7e
sadamia23 <saba777adamia@gmail..com>
2025-08-14 17:08:27 +0400

    Update Category entity

--- src/Translator.Domain/DataModels/Category.cs
+++ src/Translator.Domain/DataModels/Category.cs
@@ -1,12 +1,12 @@
 namespace Translator.Domain.DataModels;
 
-public class Category : BaseDataModel
+public class Category(string value, string type, int? order = null, Guid? parentId = null) : BaseDataModel
 {
-    public Guid? ParentId { get; init; }
-    public string Value { get; private set; }
-    public int Order { get; private set; }
-    public string Type { get; private set; }
+    public Guid? ParentId { get; init; } = parentId;
+    public string Value { get; private set; } = value;
+    public int? Order { get; private set; } = order;
+    public string Type { get; private set; } = type;
     
-    public Category? Parent { get; private set; }
-    public List<Category>? Children { get; private set; } = new();
+    public Category? Parent { get; set; }
+    public List<Category>? Children { get; set; }
 }
