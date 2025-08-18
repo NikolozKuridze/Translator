f5c0769579576b0f49af859602bd97fe08c11480
sadamia23 <saba777adamia@gmail..com>
2025-08-18 11:45:40 +0400

    Update Category entit, change setters to public

--- src/Translator.Domain/DataModels/Category.cs
+++ src/Translator.Domain/DataModels/Category.cs
@@ -2,10 +2,10 @@ namespace Translator.Domain.DataModels;
 
 public class Category(string value, string type, int? order = null, Guid? parentId = null) : BaseDataModel
 {
-    public string Value { get; private set; } = value;
-    public string Type { get; private set; } = type;
+    public string Value { get; set; } = value;
+    public string Type { get; set; } = type;
     public int? Order { get; set; } = order;
-    public Guid? ParentId { get; init; } = parentId;
+    public Guid? ParentId { get; set; } = parentId;
     
     public Category? Parent { get; set; }
     public List<Category>? Children { get; set; }
