597f232f96f0b55db6b7f063e38024b8cd25a0c8
sadamia23 <saba777adamia@gmail..com>
2025-08-18 11:48:29 +0400

    Update Create Category Command Handler

---
+++ src/Translator.Application/Features/Category/Commands/AddCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,26 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Translator.Application.Exceptions;
+using Translator.Application.Helpers;
+using Translator.Infrastructure.Database.Postgres.Repository;
+using CategoryEntity = Translator.Domain.DataModels.Category;
+
+namespace Translator.Application.Features.Category.Commands.AddCategory;
+
+public class CreateCategoryCommandHandler(
+    IRepository<CategoryEntity> _categoryRepository,
+    CategoryChecker categoryChecker) : IRequestHandler<CreateCategoryCommand, Guid>
+{
+    public async Task<Guid> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        await categoryChecker.CheckSiblingsAsync(request.Value, request.Type,
+            request.ParentId, null, cancellationToken);
+        await categoryChecker.CheckAncestorsAsync(request.Type, request.ParentId, cancellationToken);
+
+        var category = new CategoryEntity(request.Value, request.Type, request.Order, request.ParentId);
+
+        await _categoryRepository.AddAsync(category, cancellationToken);
+        await _categoryRepository.SaveChangesAsync(cancellationToken);
+        return category.Id;
+    }
+}
