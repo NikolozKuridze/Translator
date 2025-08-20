88e8aeb2e19086423c26eeda0fe3bdebbf508048
Sadamia23 <saba777adamia@gmail.com>
2025-08-20 21:07:12 +0400

    Create category constants class

---
+++ src/Translator.Infrastructure/Database/Postgres/Configurations/Constants/Constants.Type.cs
@@ -0,0 +1,9 @@
+namespace Translator.Infrastructure.Database.Postgres.Configurations.Constants;
+
+public partial class DatabaseConstants
+{
+    public static class Type
+    {
+        public const int NAME_MAX_LENGTH = 100;
+    }
+}
