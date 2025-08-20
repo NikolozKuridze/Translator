7e4eada2fa30a36a26a6379d1c676a0ea5916f3e
shaw3rm5 <shaw3rm5@users.noreply.github.com>
2025-08-20 11:24:59 +0400

    update dbcontext, inject in his constructor configuration class for LanguageSeeder.cs

--- src/Translator.Infrastructure/Database/Postgres/ApplicationDbContext.cs
+++ src/Translator.Infrastructure/Database/Postgres/ApplicationDbContext.cs
@@ -1,22 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Translator.Domain.DataModels;
+using Translator.Infrastructure.Configurations;
 using Translator.Infrastructure.Database.Postgres.SeedData;
 
 namespace Translator.Infrastructure.Database.Postgres;
 
 public class ApplicationDbContext : DbContext
 {
+    private readonly IOptions<LanguageSeedingConfiguration> _languageSeedingConfiguration;
     public DbSet<Template> Templates { get; set; }
     public DbSet<Language> Languages { get; set; }
     public DbSet<Translation> Translations { get; set; }
     public DbSet<Value> Values { get; set; }
     public DbSet<Category> Categories { get; set; }
     
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
-        : base(options) { }
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options,
+        IOptions<LanguageSeedingConfiguration> languageSeedingConfiguration) 
+        : base(options)
+    {
+        _languageSeedingConfiguration = languageSeedingConfiguration;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        _ = new LanguageSeeder(_languageSeedingConfiguration.Value).Seed(modelBuilder);
     }
 }
