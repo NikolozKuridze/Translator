bbb27416c90e84037000b179c87baa5cd3667380
shaw3rm5 <shaw3rm5@users.noreply.github.com>
2025-08-20 11:23:12 +0400

    update migrations

--- src/Translator.Infrastructure/Migrations/20250819143540_Initial.Designer.cs
+++ src/Translator.Infrastructure/Migrations/20250819143540_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Translator.Infrastructure.Database.Postgres;
@@ -11,9 +12,11 @@ using Translator.Infrastructure.Database.Postgres;
 namespace Translator.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250819143540_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
