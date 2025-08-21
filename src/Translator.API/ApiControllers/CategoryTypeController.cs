a9c05d8b0d0f5354e1d924ccba50b4a1957c5f36
sadamia23 <saba777adamia@gmail..com>
2025-08-21 14:11:56 +0400

    Add create bulk endpoint to category type controller

--- src/Translator.API/ApiControllers/CategoryTypeController.cs
+++ src/Translator.API/ApiControllers/CategoryTypeController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Translator.API.Models;
+using Translator.Application.Features.CategoryTypes.Commands.CreateBulkCategoryType;
 using Translator.Application.Features.CategoryTypes.Commands.CreateCategoryType;
 using Translator.Application.Features.CategoryTypes.Commands.DeleteCategoryType;
 using Translator.Application.Features.CategoryTypes.Queries;
@@ -18,11 +19,21 @@ public class CategoryTypeController(IMediator mediator) : ControllerBase
         
         return await mediator.Send(command);
     }
+    
+    [HttpPost("bulk")]
+    public async Task<CreateBulkCategoryTypeResponse> CreateBulk(CreateBulkCategoryTypeModel model)
+    {
+        var command = new CreateBulkCategoryTypeCommand(
+            model.TypeNames.Select(name => name.ToLower().Trim()));
+    
+        return await mediator.Send(command);
+    }
 
     [HttpDelete]
     public async Task<ActionResult> Delete(DeleteCategoryTypeModel model)
     {
-        var command = new DeleteCategoryTypeCommand(model.TypeName.ToLower().Trim());
+        var command = new DeleteCategoryTypeCommand(
+            model.TypeNames.Select(name => name.ToLower().Trim()));
         
         await mediator.Send(command);
         
