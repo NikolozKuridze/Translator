7f2284db878f9f361eadff586c03b260469c386e
Sadamia23 <saba777adamia@gmail.com>
2025-08-23 00:05:44 +0400

    Remove unused files

--- src/Translator.Application/Features/Category/Commands/UpdateCategory/UpdateCategoryCommand.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace Translator.Application.Features.Category.Commands.UpdateCategory;
-
-public sealed record UpdateCategoryCommand(
-    Guid Id,
-    string? Value,
-    int? Order): IRequest;
