55a32d323ae9708b0a81a6a7bff1f6844f93a26c
sadamia23 <saba777adamia@gmail..com>
2025-08-22 17:08:45 +0400

    Delete unused separate commands

--- src/Translator.Application/Features/CategoryTypes/Commands/CreateBulkCategoryType/CreateBulkCategoryTypeCommand.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace Translator.Application.Features.CategoryTypes.Commands.CreateBulkCategoryType;
-
-public sealed record CreateBulkCategoryTypeCommand(
-    IEnumerable<string> TypeNames) : IRequest<CreateBulkCategoryTypeResponse>;
