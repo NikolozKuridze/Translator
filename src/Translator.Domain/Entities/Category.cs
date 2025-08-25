86968df2e870074c6824434f6508f9d9fbd5c797
Sadamia23 <saba777adamia@gmail.com>
2025-08-25 20:44:59 +0400

    Add new properties to the category entity

--- src/Translator.Domain/Entities/Category.cs
+++ src/Translator.Domain/Entities/Category.cs
@@ -3,13 +3,17 @@ namespace Translator.Domain.Entities;
 public class Category(
     string value,
     Guid typeId,
+    string? metadata,
+    string? shortcode,
     int? order = null,
     Guid? parentId = null) : BaseEntity
 {
     public string Value { get; set; } = value;
     public Guid TypeId { get; init; } = typeId;
-    public CategoryType Type { get; set; } = null!;
     public int? Order { get; set; } = order;
+    public string? Metadata { get; set; }
+    public string? Shortcode { get; set; }
+    public CategoryType Type { get; set; } = null!;
     public Guid? ParentId { get; set; } = parentId;
     public Category? Parent { get; set; }
     public List<Category>? Children { get; set; }
