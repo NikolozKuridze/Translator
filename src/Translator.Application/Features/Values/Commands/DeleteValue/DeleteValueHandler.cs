d2374afe2376443def2fc2aaf7d4955009c6025f
sadamia23 <saba777adamia@gmail..com>
2025-09-02 14:36:25 +0400

    Fix bug while deleting value. Templates cannot have 0 value count

--- src/Translator.Application/Features/Values/Commands/DeleteValue/DeleteValueHandler.cs
+++ src/Translator.Application/Features/Values/Commands/DeleteValue/DeleteValueHandler.cs
@@ -8,31 +8,39 @@ using TemplateEntity = Translator.Domain.Entities.Template;
 
 namespace Translator.Application.Features.Values.Commands.DeleteValue;
 
-public class DeleteValueHandler : IRequestHandler<DeleteValueCommand>
+public class DeleteValueHandler(
+    IRepository<Value> valueRepository,
+    IRepository<TemplateEntity> templateRepository,
+    ValueCacheService valueCacheService,
+    TemplateCacheService templateCacheService)
+    : IRequestHandler<DeleteValueCommand>
 {
-    private readonly IRepository<Value> _valueRepository;
-    private readonly ValueCacheService _valueCacheService;
-
-    public DeleteValueHandler(
-        IRepository<Value> valueRepository,
-        ValueCacheService valueCacheService)
-    {
-        _valueRepository = valueRepository;
-        _valueCacheService = valueCacheService;
-    }
     public async Task Handle(DeleteValueCommand request, CancellationToken cancellationToken)
     {
         var valueNameHash = TemplateEntity.HashName(request.ValueName);
-        
-        var existsValue = await _valueRepository
-            .Where(t => t.Hash == valueNameHash) 
+
+        var existsValue = await valueRepository
+            .Where(v => v.Hash == valueNameHash)
+            .Include(v => v.Templates)
+                .ThenInclude(t => t.Values)
             .SingleOrDefaultAsync(cancellationToken);
 
         if (existsValue is null)
             throw new ValueNotFoundException(request.ValueName);
 
-        await _valueRepository.DeleteAsync([existsValue]);
-        await _valueCacheService.DeleteValueTranslationsAsync(existsValue.Id);
-        await _valueRepository.SaveChangesAsync(cancellationToken);
+        var templatesWithOneValue = existsValue.Templates
+            .Where(t => t.Values.Count <= 1)
+            .ToArray();
+        
+        if (templatesWithOneValue.Length != 0)
+        {
+            await templateRepository.DeleteAsync(templatesWithOneValue);
+            foreach (var template in templatesWithOneValue)
+                await templateCacheService.DeleteTemplateAsync(template.Id);
+        }
+
+        await valueRepository.DeleteAsync([existsValue]);
+        await valueCacheService.DeleteValueTranslationsAsync(existsValue.Id);
+        await valueRepository.SaveChangesAsync(cancellationToken);
     }
 }
