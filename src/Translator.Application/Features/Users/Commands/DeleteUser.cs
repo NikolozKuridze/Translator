2105c3957c5e736c4605ad7948ebc06b6a8b83f0
Sadamia23 <saba777adamia@gmail.com>
2025-09-18 06:19:19 +0400

    Create Delete User feature

---
+++ src/Translator.Application/Features/Users/Commands/DeleteUser.cs
@@ -0,0 +1,51 @@
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Translator.Application.Exceptions;
+using Translator.Domain.Entities;
+using Translator.Infrastructure.Database.Postgres.Configurations.Constants;
+using Translator.Infrastructure.Database.Postgres.Repository;
+
+namespace Translator.Application.Features.Users.Commands;
+
+public class DeleteUser
+{
+    public sealed record Command(string UserName) : IRequest<Response>;
+
+    public sealed record Response(Guid Id);
+    
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(x =>  x.UserName)
+                .NotEmpty()
+                .MinimumLength(DatabaseConstants.User.USERNAME_MIN_LENGTH)
+                .MaximumLength(DatabaseConstants.User.USERNAME_MAX_LENGTH);
+        }
+    }
+
+    public class Handler(
+        IRepository<User> userRepository,
+        IValidator<Command> validator
+    ) : IRequestHandler<Command, Response>
+    {
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            await validator.ValidateAndThrowAsync(request,  cancellationToken);
+            
+            var normalizedUserName = request.UserName.ToLower().Trim();
+            
+            var userExists = await userRepository.AsQueryable()
+                .FirstOrDefaultAsync(u => u.Username == normalizedUserName, cancellationToken);
+            
+            if (userExists is  null)
+                throw new UserNotFoundException(normalizedUserName);
+
+            await userRepository.DeleteAsync([userExists]);
+            await userRepository.SaveChangesAsync(cancellationToken);
+
+            return new Response(userExists.Id);
+        }
+    }
+}
