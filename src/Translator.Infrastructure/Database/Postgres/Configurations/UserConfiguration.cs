5de724c37693a66f00023bd827fbc881e1d0c8e8
Sadamia23 <saba777adamia@gmail.com>
2025-09-18 06:16:56 +0400

    Add User Entity Configurations

---
+++ src/Translator.Infrastructure/Database/Postgres/Configurations/UserConfiguration.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Translator.Domain.Entities;
+using Translator.Infrastructure.Database.Postgres.Configurations.Constants;
+
+namespace Translator.Infrastructure.Database.Postgres.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Username)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.User.USERNAME_MAX_LENGTH);
+        builder.HasIndex(u => u.Username)
+            .IsUnique();
+        
+        builder.Property(u => u.SecretKey)
+            .IsRequired()
+            .HasMaxLength(DatabaseConstants.User.SECRET_KEY_LENGTH);
+        builder.HasIndex(u => u.SecretKey)
+            .IsUnique();
+        
+        builder.HasMany(u => u.Templates)
+            .WithOne(t => t.Owner)
+            .HasForeignKey(t => t.OwnerId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(u => u.Values)
+            .WithOne(v => v.Owner)
+            .HasForeignKey(v => v.OwnerId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
