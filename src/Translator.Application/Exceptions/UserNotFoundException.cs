21078a40d67cc41332095512dcb2e780705eaa93
Sadamia23 <saba777adamia@gmail.com>
2025-09-18 13:06:58 +0400

    Overload user not found exception

--- src/Translator.Application/Exceptions/UserNotFoundException.cs
+++ src/Translator.Application/Exceptions/UserNotFoundException.cs
@@ -2,6 +2,18 @@ namespace Translator.Application.Exceptions;
 
 public class UserNotFoundException : ApplicationLayerException
 {
-    public UserNotFoundException(string userName) 
-        : base(ErrorCodes.NotFound, $"User '{userName}' does not exist.") { }
+    public UserNotFoundException(string userName)
+        : base(ErrorCodes.NotFound, $"User '{userName}' does not exist.")
+    {
+    }
+
+    public UserNotFoundException(Guid userId)
+        : base(ErrorCodes.NotFound, $"User with ID '{userId}' does not exist.")
+    {
+    }
+
+    public UserNotFoundException()
+        : base(ErrorCodes.NotFound, "User does not exist.")
+    {
+    }
 }
