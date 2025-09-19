d784a692fdce8a7274d5ca43932edc1802ab95aa
Sadamia23 <saba777adamia@gmail.com>
2025-09-19 12:23:12 +0400

    Add response types for each controller where needed.

--- src/Translator.API/ApiControllers/TemplateController.cs
+++ src/Translator.API/ApiControllers/TemplateController.cs
@@ -2,11 +2,8 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Translator.API.Attributes;
 using Translator.API.Models;
-using Translator.Application.Features.Template.Commands.CreateTemplate;
-using Translator.Application.Features.Template.Commands.DeleteTemplate;
-using Translator.Application.Features.Template.Queries.GetAllTemplates;
-using Translator.Application.Features.Template.Queries.GetTemplate;
-using Translator.Application.Features.Template.Queries.SearchTemplate;
+using Translator.Application.Features.Template.Commands;
+using Translator.Application.Features.Template.Queries;
 using Translator.Domain.Pagination;
 
 namespace Translator.API.ApiControllers;
@@ -19,56 +16,62 @@ public class TemplateController : ControllerBase
     private readonly IMediator _mediator;
 
     public TemplateController(IMediator mediator)
-        => _mediator = mediator;
-    
+    {
+        _mediator = mediator;
+    }
+
     [HttpPost("create-template")]
     public async Task<IResult> AddTemplate(
         [FromBody] CreateTemplateModel model)
     {
-        var command = new CreateTemplateCommand(
+        var command = new CreateTemplate.Command(
             model.TemplateName.Trim(), model.Values);
         await _mediator.Send(command);
-        
+
         return Results.Ok();
     }
 
     [HttpGet("search-templates")]
+    [ProducesResponseType(typeof(PaginatedResponse<GetAllTemplates.Response>), StatusCodes.Status200OK)]
     public async Task<IResult> GetTemplates(
         string templateName, int pageNumber, int pageSize)
     {
-        var command = new SearchTemplateCommand(
+        var command = new SearchTemplate.Command(
             templateName,
             new PaginationRequest(pageNumber, pageSize, null, null, null, null));
-        
+
         var result = await _mediator.Send(command);
-        
+
         return Results.Ok(result);
     }
-    
+
     [HttpGet("get-template/")]
+    [ProducesResponseType(typeof(PaginatedResponse<GetTemplate.Response>), StatusCodes.Status200OK)]
     public async Task<IResult> GetTemplate(
         [FromQuery] Guid templateId,
         [FromQuery] bool allTranslates,
         [FromQuery] string? lang = "")
     {
-        var command = new GetTemplateCommand(templateId, lang, allTranslates, new PaginationRequest(1, 1000, null, null, null, null));
+        var command = new GetTemplate.Command(templateId, lang, allTranslates,
+            new PaginationRequest(1, 1000, null, null, null, null));
         var result = await _mediator.Send(command);
         return Results.Ok(result);
     }
-    
+
     [HttpGet("get-templates/{pageNumber}/{pageSize}")]
+    [ProducesResponseType(typeof(PaginatedResponse<GetAllTemplates.Response>), StatusCodes.Status200OK)]
     public async Task<IResult> GetTemplate(int pageNumber = 1, int pageSize = 10)
     {
-        var command = new GetAllTemplatesCommand(new PaginationRequest(pageNumber, pageSize, null, null, null, null));
+        var command = new GetAllTemplates.Command(new PaginationRequest(pageNumber, pageSize, null, null, null, null));
         var result = await _mediator.Send(command);
         return Results.Ok(result);
     }
-    
+
     [HttpDelete("delete-template/{templateName}")]
     public async Task<IResult> DeleteTemplate(
         [FromRoute] string templateName)
     {
-        var command = new DeleteTemplateCommand(templateName);
+        var command = new DeleteTemplate.Command(templateName);
         await _mediator.Send(command);
         return Results.NoContent();
     }
